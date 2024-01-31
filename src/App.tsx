b7f2d4a34a0f2d7be03b22ee8e6fcb43538078ac
amandats4i <amandatsai16@gmail.com>
2024-01-31 10:19:23 -0300

    CRUD de categoria

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,29 @@ import './App.css';
 import Home from './pages/home/Home';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ListaCategorias from './components/categorias/listaCategorias/ListaCategorias';
+import DeletarCategoria from './components/categorias/deletarCategorias/DeletarCategorias';
+import FormCategorias from './components/categorias/formCategorias/FormCategorias';
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <div className="min-h-[80vh]">
-    <Home/>
-    </div>
-    
-    <Footer/>
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-screen" >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/categorias" element={<ListaCategorias />} />
+            <Route path="/cadastroCategoria" element={<FormCategorias />} />
+            <Route path="/editarCategoria/:id" element={<FormCategorias />} />
+            <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
-);
+  )
 }
 export default App;
-
